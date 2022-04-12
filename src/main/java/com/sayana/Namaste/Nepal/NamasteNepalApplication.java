85c8da22e3aaa19d432c69daf9295b37ca1376da
dr-roshyara <mathematikboy@yahoo.com>
2022-04-12 22:20:27 +0200

    project download

---
+++ src/main/java/com/sayana/Namaste/Nepal/NamasteNepalApplication.java
@@ -0,0 +1,13 @@
+package com.sayana.Namaste.Nepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamasteNepalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamasteNepalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayana/Namaste/Nepal/NamasteNepalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayana.Namaste.Nepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamasteNepalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

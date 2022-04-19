37ad789f51594da743945f27b2eeb7b46692764c
Nab Roshyara <N.Roshyara@nextrend.local>
2022-04-19 11:24:29 +0200

    spring project with namaste Controller

--- src/main/java/com/sayana/Namaste/Nepal/NamasteNepalApplication.java
+++ src/main/java/com/sayana/Namaste/Nepal/NamasteNepalApplication.java
@@ -2,8 +2,10 @@ package com.sayana.Namaste.Nepal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = { "com.sayana.Namaste.Nepal.controller" })
 public class NamasteNepalApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sayana/Namaste/Nepal/controller/NamasteNepalController.java
+++ src/main/java/com/sayana/Namaste/Nepal/controller/NamasteNepalController.java
@@ -1,15 +1,26 @@
+//first line must be the name of your pacakge 
+
 package com.sayana.Namaste.Nepal.controller;
+
+/**
+ * import files
+ */
 import org.springframework.stereotype.Controller; 
-@Controller("/namaste")
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
 /**
  * 
  * @author N.Roshyara
  *  @ scans the component Controller. 
  */
 public class NamasteNepalController {
-  public String getName() {
+  @GetMapping("/namaste")
+	public String index() {
 	System.out.println("I am Nab Raj");  
-   return 	 "first time Print";
+   return 	 "Namate hello! \n How are you ?";
 }
   
 }
